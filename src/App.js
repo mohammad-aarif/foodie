8da21ff307973619e4d9744f7b73d7b06f60724d
mohammad-aarif <arifbd354@gmail.com>
2022-06-06 11:41:08 +0600

    cart store in local storage using redux-persist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Layout from "./Views/Pages/Layout";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(store)
 function App() {
   return (
     <Provider store={store}>
-      <Layout />
+      <PersistGate persistor={persistor} >
+        <Layout />
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import cartReducer from './reducers/cartSlice'
-export default configureStore({
-    reducer: {
-        cart: cartReducer
-    }
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+
+  const rootReducer = combineReducers({
+      cart: cartReducer
+  })
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export default store
